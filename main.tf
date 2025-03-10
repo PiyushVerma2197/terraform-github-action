46990091d47f80d041f17c08e65195a0d1916208
piyush <piyush211997@gmail.com>
2025-03-10 15:23:08 +0530

    second

--- main.tf
+++ main.tf
@@ -82,3 +82,22 @@
 
   
 # }
+
+
+resource "azurerm_resource_group" "backend" {
+  name     = "tfstate-rg12"
+  location = "East US"
+ }
+
+resource "azurerm_storage_account" "backend" {
+  name                     = "tfstatebackend8750"
+  resource_group_name      = azurerm_resource_group.backend.name
+  location                 = azurerm_resource_group.backend.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+ }
+ 
+resource "azurerm_storage_container" "backend" {
+  name                 = "tfstate12"
+  storage_account_name = azurerm_storage_account.backend.name
+ }
