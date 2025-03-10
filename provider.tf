5b17a341e87c974539ddacbb5bb5212c7c17d7c5
piyush <piyush211997@gmail.com>
2025-03-10 15:50:24 +0530

    seconfff

--- provider.tf
+++ provider.tf
@@ -53,7 +53,7 @@ provider "azurerm" {
   features {}
 
   use_oidc = true
-  resource_provider_registration = "automatic"
+  resource_provider_registrations = "automatic"
 
   # This will force Terraform to pick the subscription from OIDC token
   #skip_provider_registration = true
