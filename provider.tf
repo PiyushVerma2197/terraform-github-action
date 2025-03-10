15e1659283a5a8fc5a468dd3bff8e56d6e093b96
piyush <piyush211997@gmail.com>
2025-03-10 15:05:48 +0530

    added secrets once again 2

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
    features {}
 
    use_oidc = true
-   
+   skip_provider_registration = true
 
 #    subscription_id = var.subscription_id
 #    tenant_id       = var.tenant_id
