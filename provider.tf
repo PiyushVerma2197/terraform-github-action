8b3b1c43dd78a86fd3a2a8a94dd91a54376fd537
piyush <piyush211997@gmail.com>
2025-03-10 15:15:14 +0530

    fresh

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
+# 
+
+
+
 terraform {
   required_providers {
     azurerm = {
@@ -8,33 +12,10 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
-   features {}
+  features {}
 
-   use_oidc = true
-   resource_provider_registrations = "none"
+  use_oidc = true
 
-
-#    subscription_id = var.subscription_id
-#    tenant_id       = var.tenant_id
-#    client_id       = var.client_id
-#    client_secret   = var.client_secret
-}
-
-resource "azurerm_resource_group" "backend" {
-  name     = "tfstate-rg12"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "backend" {
-  name                     = "tfstatebackend8750"
-  resource_group_name      = azurerm_resource_group.backend.name
-  location                 = azurerm_resource_group.backend.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
- 
-resource "azurerm_storage_container" "backend" {
-  name                 = "tfstate12"
-  storage_account_name = azurerm_storage_account.backend.name
+  # This will force Terraform to pick the subscription from OIDC token
+  skip_provider_registration = true
 }
