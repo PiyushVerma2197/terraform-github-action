f0865c7d10d567f318e6432223fa3de80fbf8f04
piyush <piyush211997@gmail.com>
2025-03-10 15:21:12 +0530

    second

--- provider.tf
+++ provider.tf
@@ -1,5 +1,43 @@
-# 
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "4.21.1"
+#     }
+#   }
+# }
 
+# provider "azurerm" {
+#   # Configuration options
+#    features {}
+
+#    use_oidc = true
+#    resource_provider_registrations = "none"
+
+
+# #    subscription_id = var.subscription_id
+# #    tenant_id       = var.tenant_id
+# #    client_id       = var.client_id
+# #    client_secret   = var.client_secret
+# }
+
+# resource "azurerm_resource_group" "backend" {
+#   name     = "tfstate-rg12"
+#   location = "East US"
+# }
+
+# resource "azurerm_storage_account" "backend" {
+#   name                     = "tfstatebackend8750"
+#   resource_group_name      = azurerm_resource_group.backend.name
+#   location                 = azurerm_resource_group.backend.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+ 
+# resource "azurerm_storage_container" "backend" {
+#   name                 = "tfstate12"
+#   storage_account_name = azurerm_storage_account.backend.name
+# }
 
 
 terraform {
@@ -17,5 +55,23 @@ provider "azurerm" {
   use_oidc = true
 
   # This will force Terraform to pick the subscription from OIDC token
-  #skip_provider_registration = true
+  skip_provider_registration = true
 }
+
+resource "azurerm_resource_group" "backend" {
+  name     = "tfstate-rg12"
+  location = "East US"
+ }
+
+resource "azurerm_storage_account" "backend" {
+  name                     = "tfstatebackend8750"
+  resource_group_name      = azurerm_resource_group.backend.name
+  location                 = azurerm_resource_group.backend.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+ }
+ 
+resource "azurerm_storage_container" "backend" {
+  name                 = "tfstate12"
+  storage_account_name = azurerm_storage_account.backend.name
+ }
