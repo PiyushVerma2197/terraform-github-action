b5f6b9d5611fb40bb3ed8d42aa2d959a93c8dc87
piyush <piyush211997@gmail.com>
2025-03-12 12:59:42 +0530

    made correction in backend

--- provider.tf
+++ provider.tf
@@ -1,91 +1,17 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source = "hashicorp/azurerm"
-#       version = "4.21.1"
-#     }
-#   }
-# }
-
-# provider "azurerm" {
-#   # Configuration options
-#    features {}
-
-#    use_oidc = true
-#    resource_provider_registrations = "none"
-
-
-# #    subscription_id = var.subscription_id
-# #    tenant_id       = var.tenant_id
-# #    client_id       = var.client_id
-# #    client_secret   = var.client_secret
-# }
-
-# resource "azurerm_resource_group" "backend" {
-#   name     = "tfstate-rg12"
-#   location = "East US"
-# }
-
-# resource "azurerm_storage_account" "backend" {
-#   name                     = "tfstatebackend8750"
-#   resource_group_name      = azurerm_resource_group.backend.name
-#   location                 = azurerm_resource_group.backend.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
- 
-# resource "azurerm_storage_container" "backend" {
-#   name                 = "tfstate12"
-#   storage_account_name = azurerm_storage_account.backend.name
-# }
-
-
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.21.1"
     }
   }
-  backend "azurerm" {
-    subscription_id = "e3bf1520-631c-4200-b6e7-dc6ade33c0b7"
-    resource_group_name   = "tfstate-rg12"
-    storage_account_name  = "tfstatebackend8750"
-    container_name        = "tfstate12"
-    key                    = "terraform.tfstate"
-  }
+  
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
 
   use_oidc = true
-
-  #subscription_id = "e3bf1520-631c-4200-b6e7-dc6ade33c0b7"
-  subscription_id = var.subscription_id
-  #resource_provider_registrations = ["*"]
-  #resource_provider_registrations = "all"
-
-
-
-  # This will force Terraform to pick the subscription from OIDC token
-  #skip_provider_registration = true
+  subscription_id = var.subscription_id  
 }
-
-# resource "azurerm_resource_group" "backend" {
-#   name     = "tfstate-rg12"
-#   location = "East US"
-#  }
-
-# resource "azurerm_storage_account" "backend" {
-#   name                     = "tfstatebackend8750"
-#   resource_group_name      = azurerm_resource_group.backend.name
-#   location                 = azurerm_resource_group.backend.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#  }
- 
-# resource "azurerm_storage_container" "backend" {
-#   name                 = "tfstate12"
-#   storage_account_name = azurerm_storage_account.backend.name
-#  }
